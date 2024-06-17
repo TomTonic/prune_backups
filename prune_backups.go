c9d87852589a27109ca193fe81178d359e9280fe
Tom Tonic <tom@tomtonic.de>
2024-06-17 09:59:42 +0200

    - removed a bug concerning missed directories at the juncton between hourly and daily and daily and monthly - tidy code a bit - commented the code a little - added command line parameter support - added test directory functionality

--- prune_backups.go
+++ prune_backups.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,89 +10,36 @@ import (
 	"time"
 )
 
-func createTimeStampPatterns() []string {
-	// Create an array to hold the timestamps
-	timestamps := make([]string, 24+30+120)
-
-	// Get the current time
-	t := time.Now()
-
-	// Generate the timestamps
-	for i := 0; i < 24; i++ {
-		// Format the time in the format YYYY-MM-DD_hh
-		timestamps[i] = t.Format("2006-01-02_15")
-
-		// Subtract one hour from the current timestamp
-		t = t.Add(-1 * time.Hour)
-	}
-
-	// Subtract one day from the current timestamp
-	t = t.Add(-24 * time.Hour)
-
-	for i := 24; i < 24+30; i++ {
-		// Format the time in the format YYYY-MM-DD
-		timestamps[i] = t.Format("2006-01-02")
-
-		// Subtract one day from the current timestamp
-		t = t.Add(-24 * time.Hour)
-	}
-
-	// Subtract one month from the current timestamp
-	t = t.AddDate(0, -1, 0)
-
-	for i := 24 + 30; i < 24+30+120; i++ {
-		// Format the time in the format YYYY-MM
-		timestamps[i] = t.Format("2006-01")
-
-		// Subtract one month from the current timestamp
-		t = t.AddDate(0, -1, 0)
-	}
-
-	return timestamps
-}
-
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Please provide a directory path")
+
+	/* Processing command line parameters */
+
+	pruneDirName := flag.String("dir", "<none>", "REQUIRED. The name of the directory that shall be pruned.")
+	toDeleteDirName := flag.String("to_directory", "to_delete", "OPTIONAL. The name of the directory where the pruned directories shall be moved.")
+	doTest := flag.Bool("test", false, "OPTIONAL. Generate test directories and prune them afterwards if `true`. (default false)") // caution: go will neither print the type nor the default for bool flags with default false. see https://github.com/golang/go/issues/63150
+
+	flag.CommandLine.SetOutput(os.Stdout)
+	flag.Parse()
+
+	// workaroud as REQUIRED parameters are not supported by the flag package
+	if !isFlagPassed("dir") {
+		fmt.Println("Pruning directory missing (-dir).")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	dirPath := os.Args[1]
 
-	/*
-		// generate random test directories
+	// generates 2800 hourly directories (24h x 30d x 4months)
+	if *doTest {
+		generateTestDirectories(*pruneDirName, 2800)
+	}
 
-		_, err := os.ReadDir(dirPath)
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Trying to create it...")
-			// The second argument is the permission mode.
-			// 0755 commonly used for directories.
-			err := os.MkdirAll(dirPath, 0755)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		} else {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	/* Reading and filtering directories */
 
-		now := time.Now()
-		for i := 0; i < 100; i++ {
-			randomNumber := rand.Intn(438) + 1
-			t := now.Add(time.Duration(-randomNumber) * time.Hour)
-			subDir := t.Format("2006-01-02_15-04")
-			fullPath := filepath.Join(dirPath, subDir)
-			err := os.MkdirAll(fullPath, 0755)
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-	*/
-
-	files, err := os.ReadDir(dirPath)
+	files, err := os.ReadDir(*pruneDirName)
 	if err != nil {
+		fmt.Print("Could not read pruning directory (-dir): ")
 		fmt.Println(err)
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
@@ -101,30 +49,32 @@ func main() {
 			dirs = append(dirs, file.Name())
 		}
 	}
-	fmt.Println("I found", len(dirs), "directories in", dirPath)
+	fmt.Println("I found", len(dirs), "directories in", *pruneDirName)
 
-	// Sort in descending order
+	// Sort in descending order - caution: this is important for the algorithm!
 	sort.Sort(sort.Reverse(sort.StringSlice(dirs)))
 
-	/*
-		fmt.Println("Found directories:")
-		for _, dir := range dirs {
-			fmt.Println(dir)
-		}
-	*/
+	now := time.Now()
+	var to_delete []string // in this array we will collect all directories that we will move to the to_delete-directory
 
-	prefixes := createTimeStampPatterns()
-
-	var to_delete []string
-	for _, prefix := range prefixes {
-		add_to_delete := all_but_first_matching_prefix(dirs, prefix)
+	prefixesForTimeSlotsToKeepOne := createPrefixesForTimeSlotsToKeepOne(now)
+	for _, prefix := range prefixesForTimeSlotsToKeepOne {
+		add_to_delete := getAllButFirstMatchingPrefix(dirs, prefix)
 		to_delete = append(to_delete, add_to_delete...)
 	}
 
-	delPath := filepath.Join(dirPath, "to_delete")
+	prefixesForTimeSlotsToKeepNone := createPrefixesForTimeSlotsToKeepNone(now)
+	for _, prefix := range prefixesForTimeSlotsToKeepNone {
+		add_to_delete := getAllMatchingPrefix(dirs, prefix)
+		to_delete = append(to_delete, add_to_delete...)
+	}
+
+	/* now we have collected all directory names that need to be moved in to_delete. next we will create the target directory and actually move them */
+
+	delPath := filepath.Join(*pruneDirName, *toDeleteDirName)
 	err2 := os.MkdirAll(delPath, 0755)
 	if err2 != nil {
-		fmt.Print("Error creating 'to_delete'-directory: ")
+		fmt.Print("Error creating directory \"", delPath, "\": ")
 		fmt.Println(err)
 		fmt.Println("I woud have moved the following directories there:")
 		for _, dir := range to_delete {
@@ -135,7 +85,7 @@ func main() {
 
 	var successful_move_counter = 0
 	for _, dirname := range to_delete {
-		fromPath := filepath.Join(dirPath, dirname)
+		fromPath := filepath.Join(*pruneDirName, dirname)
 		toPath := filepath.Join(delPath, dirname)
 		fmt.Print("Moving ", fromPath, " to ", toPath, "... ")
 		err3 := os.Rename(fromPath, toPath)
@@ -149,7 +99,17 @@ func main() {
 	fmt.Println("I moved", successful_move_counter, "directories to", delPath)
 }
 
-func all_but_first_matching_prefix(from []string, prefix string) []string {
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
+func getAllButFirstMatchingPrefix(from []string, prefix string) []string {
 	var result []string
 	var first = true
 	for _, s := range from {
@@ -162,4 +122,118 @@ func all_but_first_matching_prefix(from []string, prefix string) []string {
 		}
 	}
 	return result
+}
+
+func getAllMatchingPrefix(from []string, prefix string) []string {
+	var result []string
+	for _, s := range from {
+		if strings.HasPrefix(s, prefix) {
+			result = append(result, s)
+		}
+	}
+	return result
+}
+
+func createPrefixesForTimeSlotsToKeepOne(current time.Time) []string {
+	// Create an array to hold the prefixes
+	prefixes := make([]string, 24+30+120)
+
+	// Generate the timestamps
+	for i := 0; i < 24; i++ {
+		// Format the time in the format YYYY-MM-DD_hh
+		prefixes[i] = current.Format("2006-01-02_15") // caution, this is a magic number in go!
+
+		// Subtract one hour from the current timestamp
+		current = current.Add(-1 * time.Hour)
+	}
+
+	// Subtract one day from the current timestamp
+	current = current.Add(-24 * time.Hour)
+
+	for i := 24; i < 24+30; i++ {
+		// Format the time in the format YYYY-MM-DD
+		prefixes[i] = current.Format("2006-01-02")
+
+		// Subtract one day from the current timestamp
+		current = current.Add(-24 * time.Hour)
+	}
+
+	// Subtract one month from the current timestamp
+	current = current.AddDate(0, -1, 0)
+
+	for i := 24 + 30; i < 24+30+120; i++ {
+		// Format the time in the format YYYY-MM
+		prefixes[i] = current.Format("2006-01")
+
+		// Subtract one month from the current timestamp
+		current = current.AddDate(0, -1, 0)
+	}
+
+	/*
+		for _, prefix := range prefixes {
+			fmt.Println(prefix)
+		}
+	*/
+
+	return prefixes
+}
+
+func createPrefixesForTimeSlotsToKeepNone(current time.Time) []string {
+	// Create an array to hold the prefixes
+	var prefixes []string
+
+	// keep first 24h
+	current = current.Add(-24 * time.Hour)
+	dayOfLastHourlyBackup := current.Day()
+
+	// add all hours betwen dayOfLastHourlyBackup and the next day/date where we keep the first daily backup
+	for current.Day() == dayOfLastHourlyBackup {
+		prefixes = append(prefixes, current.Format("2006-01-02_15"))
+		current = current.Add(-1 * time.Hour)
+	}
+
+	// keep 30 daily backups
+	current = current.Add(30 * -24 * time.Hour)
+
+	dayOfLastMonthlyBackup := current.Month()
+
+	// add all days betwen +24h and the next day/date
+	for current.Month() == dayOfLastMonthlyBackup {
+		prefixes = append(prefixes, current.Format("2006-01-02"))
+		current = current.Add(-24 * time.Hour)
+	}
+
+	/*
+		for _, prefix := range prefixes {
+			fmt.Println(prefix)
+		}
+	*/
+	return prefixes
+}
+
+func generateTestDirectories(dirPath string, number int) {
+
+	_, err := os.ReadDir(dirPath)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Trying to create it...")
+		// The second argument is the permission mode.
+		// 0755 commonly used for directories.
+		err := os.MkdirAll(dirPath, 0755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	now := time.Now()
+	for i := 0; i < number; i++ {
+		t := now.Add(time.Duration(-i) * time.Hour)
+		subDir := t.Format("2006-01-02_15-04")
+		fullPath := filepath.Join(dirPath, subDir)
+		err := os.MkdirAll(fullPath, 0755)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
