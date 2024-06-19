dd770cca689238e7b8325a766906e1ef08e66ad1
Tom Tonic <tom@tomtonic.de>
2024-06-19 16:56:50 +0200

    documented corner cases

--- prune_backups.go
+++ prune_backups.go
@@ -168,6 +168,45 @@ func getAllFilters(current_time time.Time, allDirs []string) []string {
 	remaining_hourlys := 24 - len(filters_today)
 	filters_yesterday := getFiltersForYesterday(current_time, remaining_hourlys, allDirs)
 	result := append(filters_today, filters_yesterday...)
+
+	/*
+		The 24h-logic affects two days - today and yesterday. The 30 daily backups affect 30 days before that. This sums up to 32 days. There are two cases:
+		a) The 32 days affect two months M0 and M1. M0 is the month that contains today. M1 is the month where the 30st daily backup lies.
+			- Assertion: M0 is completely covered by hourly and/or daily backups.
+			i) M1 is completely covered with hourly and/or daily backups.
+				- In this case we may not use an extra "(only-)keep-the-newest-of-the-month"-filter.
+				- The monthly filters start from M2
+				- This is the case if
+					* day(today) = 4 && daycount(M1) = 28, or
+					* day(today) = 3 && daycount(M1) = 29, or
+					* day(today) = 2 && daycount(M1) = 30, or
+					* day(today) = 1 && daycount(M1) = 31
+					* OR: day(today) + daycount(M1) = 32
+			ii) M1 is not completely covered with daily backups.
+				- Assertion: M1 is only affected by daily filters: Even 31-day-months would completely be covered if some hourly filters of the second day would spill into the month.
+				- In this case we need an extra "(only-)keep-the-newest-of-the-month"-filter <=> (if and only if) there are no actual matches for daily filters in M1.
+				- The monthly filters start from M2
+				- This is the case if
+					* day(today) > 4, or
+					* day(today) = 4 && daycount(M1) > 28, or
+					* day(today) = 3 && daycount(M1) > 29, or
+					* day(today) = 2 && daycount(M1) > 30, or
+					* day(today) = 1 && daycount(M1) > 31 (impossible)
+					* OR: day(today) + daycount(M1) > 32
+		b) The 32 days affect three months M0, M1, and M2. M0 is the month that contains today. M2 is the month where the 30st daily backup lies.
+			- This is the case if
+				* day(today) = 3 && daycount(M1) <= 28, or
+				* day(today) = 2 && daycount(M1) <= 29, or
+				* day(today) = 1 && daycount(M1) <= 30
+				* OR: day(today) + daycount(M1) < 32
+			- Assertion: M0 is completely covered by hourly and/or daily backups.
+			- Assertion: M1 is a month with less than 31 days.
+			- Assertion: M1 is completely covered by hourly and/or daily backups.
+			- Assertion: M2 is not completely covered with hourly and/or daily backups.
+			ii) M2 is not completely covered with daily backups.
+				- In this case we need an extra "(only-)keep-the-newest-of-the-month"-filter <=> (if and only if) there are no actual matches for daily filters in M2.
+				- The monthly filters start from M3
+	*/
 	filters_30days := getFiltersFor30Dailys(current_time)
 	result = append(result, filters_30days...)
 	need_an_extra_monthly, for_month := getMonthToLookForAnExtraMonthly(current_time)
