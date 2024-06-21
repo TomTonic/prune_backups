4555bc458f82ea4641aa2cf5b1b8a7e8e3ce22bc
Tom Tonic <tom@tomtonic.de>
2024-06-21 14:51:34 +0200

    excluded darwin from this feature as the necessary low level functions are not (yet?) available

--- stats_other.go
+++ stats_other.go
@@ -1,4 +1,4 @@
-//go:build !(linux || darwin || windows)
+//go:build !(linux || windows)
 
 package main
 
@@ -8,6 +8,6 @@ import (
 )
 
 func getSizeAndLinkCount(filename string) (size, link_count uint64, err error) {
-	err = fmt.Errorf("getSizeAndLinkCount not implemented for this OS (%s)", runtime.GOOS)
+	err = fmt.Errorf("Sorry, the necessary low level file system operations are not implemented for this this operating system (%s).", runtime.GOOS)
 	return 0, 0, err
 }
